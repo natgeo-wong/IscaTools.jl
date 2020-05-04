8b38ddf8a3f1fdcb46e4a2173f993ad45754d3ae
Nathanael Wong <natgeo.wong@outlook.com>
2020-05-04 00:23:59 -0400

    Files generated by PkgTemplates

---
+++ src/IscaTools.jl
@@ -0,0 +1,5 @@
+module IscaTools
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using IscaTools
+using Test
+
+@testset "IscaTools.jl" begin
+    # Write your own tests here.
+end
