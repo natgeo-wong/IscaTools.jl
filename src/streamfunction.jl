2f3a83972c90a4b45a373967f97153826c197b10
Nathanael Wong <natgeo.wong@outlook.com>
2020-05-11 11:30:53 -0400

    Debugged and added logging.

--- src/streamfunction.jl
+++ src/streamfunction.jl
@@ -2,35 +2,42 @@
 
 function iscastreamfunction(init::AbstractDict,iroot::AbstractDict)
 
-    for irun = 1 : init["nruns"]; iscacalcpsi(iroot,irun,init["sealp"]) end
+    for irun = 1 : init["nruns"]
+        @info "$(Dates.now()) - Calculating Zonal-Averaged Meridional Streamfunction for RUN $irun"
+        iscacalcpsi(iroot,irun,init["sealp"])
+    end
 
 end
 
 function iscacalcpsi(iroot::AbstractDict,irun::Integer,sealp::Real)
 
+    @info "$(Dates.now()) - Extracting Meridional Wind and Pressure data ..."
     inc = iscarawname(iroot,irun=irun); ids = Dataset(inc);
-    vwind = ids["vcomp"][:]*1; pfull = vcat(0,ids["pfull"][:]*1); lat = ids["lat"][:]*1;
+    vwind = ids["vcomp"][:]*1; pfull = vcat(0,ids["pfull"][:]*100); lat = ids["lat"][:]*1;
     close(ids)
 
+    @info "$(Dates.now()) - Reshape / Permutedims data for calculation ..."
     nlon,nlat,npre,nt = size(vwind);
-    vwind = permutdims(dropdims(mean(vwind,dims=1),dims=1),[1,3,2]);
-    vpsi = zeros(nlat,nt,npre); vii = zeros(np+1); psiii = zeros(np+1)
+    vwind = permutedims(dropdims(mean(vwind,dims=1),dims=1),[1,3,2]);
+    vpsi = zeros(nlat,nt,npre); vii = zeros(npre+1); psiii = zeros(npre+1)
 
+    @info "$(Dates.now()) - Performing Numerical Integration over Pressure Levels ..."
     for it = 1 : nt, ilat = 1 : nlat
 
-        for ipre = 1 : npre; vii[ip+1] = vwind[ilat,it,ipre] end
+        for ipre = 1 : npre; vii[ipre+1] = vwind[ilat,it,ipre] end
         psiii .= cumul_integrate(pfull,vii)
         for ipre = 1 : npre
-            vpsi[ilat,it,:] = psiii[ipre+1] * 2 * pi .* cosd.(lat[ilat]) / 9.81
+            vpsi[ilat,it,ipre] = psiii[ipre+1] * 2 * pi * 6378e3 .* cosd.(lat[ilat]) / 9.81
         end
 
     end
 
+    @info "$(Dates.now()) - Saving Meridional Streamfunction data ..."
     vpsi = permutedims(vpsi,[1,3,2]); iscasavepsi(vpsi,iroot,irun)
 
 end
 
-function iscasavepsi(vpsi::Array{<:Real,4},iroot::AbstractDict,irun::Integer)
+function iscasavepsi(vpsi::Array{<:Real,3},iroot::AbstractDict,irun::Integer)
 
     inc = iscarawname(iroot,irun=irun); ids = Dataset(inc,"a");
 
@@ -38,8 +45,6 @@ function iscasavepsi(vpsi::Array{<:Real,4},iroot::AbstractDict,irun::Integer)
         "long_name"     => "Meridional Streamfunction",
         "units"         => "kg/s",
         "scale_factor"  => 1e9,
-        "missing_value" => 1.0e20,
-        "_FillValue"    => 1.0e20,
         "cell_methods"  => "time: mean",
         "time_avg_info" => "average_T1,average_T2,average_DT",
     ))
